49e8c277c3e390721e6d374b9d26c76278ec4487
Huang Qi <huangqiyx@gmail.com>
2012-08-20 01:02:55 +0800

    Add several join test cases

--- src/test/regress/sql/tablesample.sql
+++ src/test/regress/sql/tablesample.sql
@@ -25,7 +25,32 @@ INSERT INTO sample VALUES (18, 'david');
 INSERT INTO sample VALUES (19, 'job');
 INSERT INTO sample VALUES (20, 'neil');
 
+CREATE TABLE course (id INT, course text);
+
+INSERT INTO course VALUES (1, 'Physics');
+INSERT INTO course VALUES (2, 'Computing');
+INSERT INTO course VALUES (2, 'Math');
+INSERT INTO course VALUES (3, 'Quantum Mechanics');
+INSERT INTO course VALUES (5, 'Life Science');
+INSERT INTO course VALUES (6, 'Food Science');
+INSERT INTO course VALUES (1, 'Math');
+INSERT INTO course VALUES (7, 'Music');
+INSERT INTO course VALUES (3, 'Social Science');
+INSERT INTO course VALUES (8, 'Material Science');
+INSERT INTO course VALUES (9, 'Computing');
+INSERT INTO course VALUES (10, 'Computing');
+INSERT INTO course VALUES (12, 'Physics');
+INSERT INTO course VALUES (13, 'Social Science');
+INSERT INTO course VALUES (14, 'Physics');
+INSERT INTO course VALUES (15, 'Music');
+INSERT INTO course VALUES (16, 'Math');
+INSERT INTO course VALUES (17, 'Chemistry');
+INSERT INTO course VALUES (14, 'Math');
+INSERT INTO course VALUES (18, 'Math');
+INSERT INTO course VALUES (20, 'Computing');
+
 ANALYZE sample;
+ANALYZE course;
 
 SELECT * FROM sample TABLESAMPLE SYSTEM (60) REPEATABLE (10);
 
@@ -47,6 +72,16 @@ SELECT * FROM sample TABLESAMPLE SYSTEM (65) REPEATABLE (20);
 
 SELECT * FROM sample TABLESAMPLE BERNOULLI (70) REPEATABLE (11) order by id limit 5;
 
+SELECT * FROM 
+	(SELECT * FROM sample TABLESAMPLE BERNOULLI (70) REPEATABLE (10)) s,
+    course c
+WHERE s.id = c.id;
+
+SELECT * FROM 
+	(SELECT * FROM sample TABLESAMPLE BERNOULLI (60) REPEATABLE (5)) s,
+	(SELECT * FROM course TABLESAMPLE SYSTEM (50) REPEATABLE (11)) c
+WHERE s.id = c.id;
+
 SELECT * INTO tmp FROM sample TABLESAMPLE BERNOULLI (4);
 
 DROP TABLE tmp;
@@ -55,4 +90,13 @@ SELECT * INTO tmp FROM sample TABLESAMPLE SYSTEM (20);
 
 DROP TABLE tmp;
 
+SELECT * INTO tmp FROM
+	(SELECT s.id, s.name, c.course FROM
+			(SELECT * FROM sample TABLESAMPLE BERNOULLI (60)) s,
+			(SELECT * FROM course TABLESAMPLE SYSTEM (50)) c
+	 WHERE s.id = c.id) t;
+
+DROP TABLE tmp;
+
 DROP TABLE sample;
+DROP TABLE course;
